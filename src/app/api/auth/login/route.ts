b4719e4409235fbfbccd8e0446e4c5e0b4e909d7
Adrian Bieniek <a.bieniek.kontakt@gmail.com>
2023-05-31 02:34:30 +0200

    created login api with token

--- src/app/api/auth/login/route.ts
+++ src/app/api/auth/login/route.ts
@@ -1,8 +1,26 @@
-import { NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
+import bcrypt from 'bcrypt';
+
 import dbConnection from '@/db/dbConnection';
 
-export function GET() {
+import userModel from '@/db/models/user-model';
+
+import generateToken from '@/utils/backend/generateToken';
+
+export const POST = async (req: NextRequest) => {
 	dbConnection();
-	console.log('działa');
-	return NextResponse.json({ id: 2 });
-}
+
+	const { username, password } = await req.json();
+
+	const user = await userModel.findOne({ username });
+
+	if (!user) return NextResponse.json({ communicate: 'Użytkownik nie istnieje.' });
+
+	if (bcrypt.compareSync(password, user.password)) {
+		const token = generateToken({ username, password });
+
+		return NextResponse.json({ user, token });
+	} else {
+		return NextResponse.json({ communicate: 'Błędne hasło.' });
+	}
+};
--- src/db/dbConnection.ts
+++ src/db/dbConnection.ts
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 const dbConnection = () => {
 	mongoose.set('strictQuery', true);
 	mongoose
-		.connect('mongodb+srv://admin:002NvFy004o50qls@cluster0.38cgxvs.mongodb.net/cv-generator')
+		.connect(process.env.DATABASE || 'mongodb+srv://admin:002NvFy004o50qls@cluster0.38cgxvs.mongodb.net/cv-generator')
 		.then(() => console.log('Connected to DATABASE'))
 		.catch((error) => {
 			console.log('Failed connection to DATABASE');
